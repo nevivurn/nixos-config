2e79941a7db0a02abe777da30e5e530edc8d407b
nevivurn <nevivurn@nevi.dev>
2022-12-08 22:11:44 +0900

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-22.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager } @ inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      formatter.${system} = pkgs.nixpkgs-fmt;
+
+      homeConfigurations = {
+        shell = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./home/profiles/shell
+          ];
+        };
+      };
+    };
+}
---
+++ home/profiles/shell/default.nix
@@ -0,0 +1,110 @@
+{ lib, config, pkgs, ... }:
+
+{
+  home.stateVersion = "22.11";
+  home.username = "nevivurn";
+  home.homeDirectory = "/home/nevivurn";
+
+  home.packages = with pkgs; [
+    file
+    pv
+    tree
+    zbar
+
+    unixtools.xxd
+
+    wget
+    curl
+
+    p7zip
+    unzip
+
+    ldns
+    mtr
+  ];
+
+  home.sessionVariables = {
+    EDITOR = "vim";
+  };
+
+  home.shellAliases = {
+    ls = "ls --color=tty";
+    ll = "ls -l";
+    la = "ls -A";
+    l = "ls -alh";
+    grep = "grep --color=tty";
+  };
+
+  programs.home-manager.enable = true;
+
+  editorconfig = {
+    enable = true;
+    settings = {
+      "*" = {
+        charset = "utf-8";
+        end_of_line = "lf";
+        trim_trailing_whitespace = "true";
+        insert_final_newline = "true";
+      };
+      "*.md" = {
+        max_line_length = 80;
+      };
+    };
+  };
+
+  programs.neovim = {
+    enable = true;
+    extraConfig = ''
+      set mouse=
+      set relativenumber
+
+      packadd! dracula-vim
+      set termguicolors
+      colorscheme dracula
+      hi Normal guibg=NONE ctermbg=NONE
+
+      lua << EOF
+        require'nvim-treesitter.configs'.setup {
+          highlight = {
+            enable = true,
+            disable = { "bash" },
+          },
+          indent = { enable = true },
+        }
+      EOF
+    '';
+    extraPackages = with pkgs; [ ];
+    plugins = with pkgs.vimPlugins; [
+      dracula-vim
+      editorconfig-nvim
+      nvim-treesitter.withAllGrammars
+    ];
+
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+  };
+
+  programs.bash.enable = true;
+  programs.tmux.enable = true;
+
+  programs.dircolors.enable = true;
+  programs.starship.enable = true;
+
+  programs.jq.enable = true;
+  programs.less.enable = true;
+  programs.lesspipe.enable = true;
+  programs.man.enable = true;
+
+  programs.git = {
+    enable = true;
+    aliases = {
+      graph = "log --graph --all --oneline";
+    };
+    extraConfig = {
+      init.defaultBranch = "master";
+    };
+    userName = "Yongun Seong";
+    userEmail = "nevivurn@nevi.dev";
+  };
+}
