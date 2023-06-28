cd5667eb0613fd03126ea4f16405d89d4834ffda
Yongun Seong <nevivurn@nevi.dev>
2023-06-29 01:10:24 +0900

    feat(athebyne): space out smartd tests

--- systems/athebyne/services/smartd.nix
+++ systems/athebyne/services/smartd.nix
@@ -1,6 +1,41 @@
+{ lib, ... }:
+
+let
+  disks = [
+    "ata-WDC_WUH721414ALE6L4_9JJ0DJWT"
+    "ata-WDC_WUH721414ALE6L4_9JJ0BPGT"
+
+    "ata-WDC_WD140EDGZ-11B2DA2_2CG52M7R"
+    "ata-WDC_WUH721414ALE6L4_9JHNKSGT"
+
+    "ata-WDC_WD140EDGZ-11B1PA0_9MHXTA2U"
+    "ata-WDC_WUH721414ALE6L4_9JGLSUVT"
+
+    "ata-WDC_WD140EDGZ-11B1PA0_Y6GV1N1C"
+    "ata-WDC_WUH721414ALE6L4_9JHWP7AT"
+
+    "nvme-Samsung_SSD_970_EVO_Plus_500GB_S4EVNM0T210690N"
+  ];
+  dlen = builtins.length disks;
+
+  mod = a: n: if a >= n then mod (a - n) n else a;
+  modHr = a: mod a 24;
+
+  padz = n: s: if (builtins.stringLength s) >= n then s else padz n ("0" + s);
+  pad2n = n: padz 2 (builtins.toString n);
+in
+
 {
   services.smartd = {
     enable = true;
-    defaults.autodetected = "-a -s (S/../.././00|L/../15/./12)";
+    autodetect = false;
+    devices = lib.imap0
+      (i: v: {
+        device = "/dev/disk/by-id/${v}";
+        options = "-a -o on -S on -s (" +
+          "S/../.././${pad2n (modHr (24 / dlen * i))}|" +
+          "L/../15/./${pad2n (modHr (12 + 24 / dlen * i))})";
+      })
+      disks;
   };
 }
