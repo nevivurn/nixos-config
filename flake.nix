4ea3d19c78f7e7886f1740837d6eb24dc3ebf2fa
Yongun Seong <nevivurn@nevi.dev>
2023-07-03 22:43:22 +0900

    feat(ci): combine nixos builds to checks

--- flake.nix
+++ flake.nix
@@ -40,6 +40,9 @@
         };
       };
 
+      checks.${system} =
+        builtins.mapAttrs (k: v: v.config.system.build.toplevel) self.nixosConfigurations;
+
       nixosConfigurations = {
         alrakis = nixpkgs.lib.nixosSystem {
           inherit system;
