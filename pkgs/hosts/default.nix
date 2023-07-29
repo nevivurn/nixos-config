2e32a016cdcc009e84228b5991de2fe159578093
Yongun Seong <nevivurn@nevi.dev>
2023-07-29 18:59:01 +0900

    feat(pkgs/hosts): 3.13.15 -> 3.13.16

--- pkgs/hosts/default.nix
+++ pkgs/hosts/default.nix
@@ -2,13 +2,13 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "hosts";
-  version = "3.13.15";
+  version = "3.13.16";
 
   src = fetchFromGitHub {
     owner = "StevenBlack";
     repo = "hosts";
     rev = version;
-    hash = "sha256-jhdip3auFYnkXxcndsQnNCzn1cbRZfWSOYVlVOcdgOU=";
+    hash = "sha256-qhhr28Vl8xkszDLt21ThOuJ5A5vLRVhSlLrsN4rWGAk=";
   };
 
   patches = [ ./remove-invalid.patch ];
