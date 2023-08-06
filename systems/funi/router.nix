8b2ba4cba76c4ad42e24f506c4d34718a2700551
Yongun Seong <nevivurn@nevi.dev>
2023-08-06 18:26:17 +0900

    feat(funi): use ACS

--- systems/funi/router.nix
+++ systems/funi/router.nix
@@ -107,7 +107,7 @@
       local_pwr_constraint=3
 
       hw_mode=a
-      channel=116
+      channel=acs_survey
 
       preamble=1
 
@@ -118,7 +118,6 @@
       ieee80211ac=1
       vht_capab=[MAX-MPDU-11454][RXLDPC][SHORT-GI-80][TX-STBC-2BY1][MAX-A-MPDU-LEN-EXP3][RX-ANTENNA-PATTERN][TX-ANTENNA-PATTERN]
       vht_oper_chwidth=1
-      vht_oper_centr_freq_seg0_idx=122
 
       disassoc_low_ack=1
       uapsd_advertisement_enabled=1
