ccb3016b54dcb84fc5d37535586bc51724af37a0
Yongun Seong <nevivurn@nevi.dev>
2023-09-18 12:49:45 +0900

    hosts: 3.14.5 -> 3.14.6
    Diff: https://github.com/StevenBlack/hosts/compare/3.14.5...3.14.6

--- pkgs/hosts/default.nix
+++ pkgs/hosts/default.nix
@@ -2,13 +2,13 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "hosts";
-  version = "3.14.5";
+  version = "3.14.6";
 
   src = fetchFromGitHub {
     owner = "StevenBlack";
     repo = "hosts";
     rev = version;
-    hash = "sha256-NWu6K+fXrTxXBffHtrtleLjaB0vvQmbp7SPbIj8wQFI=";
+    hash = "sha256-StUxxtxxeETwFcGqiWpMX2CIz6o4pXa3qR1kr9Z6/lc=";
   };
 
   installPhase = ''
