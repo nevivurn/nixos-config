8f8abec7d20d4543aa55947192bfa79e9b054951
Yongun Seong <nevivurn@nevi.dev>
2023-09-22 21:45:37 +0900

    Revert "feat(funi): remove root keys"
    This reverts commit 2485996f0aa372e37ac9e7a86622fb7fdd41c5f4.

--- systems/funi/default.nix
+++ systems/funi/default.nix
@@ -72,6 +72,12 @@ in
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJJ1U9//g+W2pRNdBaiADCMhAWlfWt3Ha1zwfR+iwMoZ nevivurn@tianyi"
       ];
     };
+    root = {
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILUNr1fMh1l/hCfs/hjeT3AhBESCVq3QXgbQh/cTVRS3 nevivurn@taiyi"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJJ1U9//g+W2pRNdBaiADCMhAWlfWt3Ha1zwfR+iwMoZ nevivurn@tianyi"
+      ];
+    };
   };
 
   nixpkgs.overlays = [
