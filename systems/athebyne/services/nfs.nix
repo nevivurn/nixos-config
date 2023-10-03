a8af10db0f4bd20c9164e569de96506ce08d63cb
Yongun Seong <nevivurn@nevi.dev>
2023-10-04 05:29:29 +0900

    feat(athebyne): more correct fw for nfs

--- systems/athebyne/services/nfs.nix
+++ systems/athebyne/services/nfs.nix
@@ -1,5 +1,20 @@
+{ lib, config, ... }:
+
+let
+  cfg = config.services.nfs.server;
+  allowedPorts = [ 111 2049 ]
+    ++ lib.optionals (cfg.statdPort != null) [ cfg.statdPort ]
+    ++ lib.optionals (cfg.lockdPort != null) [ cfg.lockdPort ]
+    ++ lib.optionals (cfg.mountdPort != null) [ cfg.mountdPort ];
+in
+
 {
-  services.nfs.server.enable = true;
-  networking.firewall.allowedTCPPorts = [ 111 2049 ];
-  networking.firewall.allowedUDPPorts = [ 111 ];
+  services.nfs.server = {
+    enable = true;
+    statdPort = 4000;
+    lockdPort = 4001;
+    mountdPort = 4002;
+  };
+  networking.firewall.allowedTCPPorts = allowedPorts;
+  networking.firewall.allowedUDPPorts = allowedPorts;
 }
