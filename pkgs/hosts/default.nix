c02ab55110fadb3ab73cfd69e895907257aa79c9
Yongun Seong <nevivurn@nevi.dev>
2023-11-04 19:36:59 +0900

    hosts: 3.14.22 -> 3.14.23
    Diff: https://github.com/StevenBlack/hosts/compare/3.14.22...3.14.23

--- pkgs/hosts/default.nix
+++ pkgs/hosts/default.nix
@@ -2,13 +2,13 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "hosts";
-  version = "3.14.22";
+  version = "3.14.23";
 
   src = fetchFromGitHub {
     owner = "StevenBlack";
     repo = "hosts";
     rev = version;
-    hash = "sha256-G0mj9HQDPyGetTVGP2gJpUbd9QM/ufHi2ynlN47EsGg=";
+    hash = "sha256-H1KNt1tQpHvJjTZIh90KRvHZH0Waj+f+sm6rYTz9kGI=";
   };
 
   installPhase = ''
