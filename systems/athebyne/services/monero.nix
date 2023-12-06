6d984a3d87d7c5f7bc868cc6343a8e136b70bd1b
Yongun Seong <nevivurn@nevi.dev>
2023-12-06 16:39:42 +0900

    feat(athebyne): fix node

--- systems/athebyne/services/monero.nix
+++ systems/athebyne/services/monero.nix
@@ -1,3 +1,5 @@
+{ lib, pkgs, ... }:
+
 {
   services.monero = {
     enable = true;
@@ -5,12 +7,19 @@
       no-igd=1
       no-zmq=1
 
+      zmq-pub=tcp://127.0.0.1:18083
+
       out-peers=32
       in-peers=64
 
       prune-blockchain=1
       sync-pruned-blocks=1
+
+      confirm-external-bind=1
+      restricted-rpc=1
     '';
+
+    rpc.address = "0.0.0.0";
   };
 
   systemd.services.monero.serviceConfig = {
@@ -40,9 +49,54 @@
     SystemCallFilter = "@system-service";
   };
 
-  networking.firewall.allowedTCPPorts = [ 18080 ];
+  systemd.services.monero-p2pool = {
+    description = "Decentralized pool for Monero mining";
+    after = [ "network.target" ];
+    before = [ "monero.service" ];
+    requires = [ "monero.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      User = "monero-p2pool";
+      Group = "monero-p2pool";
+      ExecStart = "${lib.getExe pkgs.p2pool} --wallet 44nMXAaeWu26HnPSDXXNWWCcr2Bs9uBZtfd2r72VyjAbXKCFWBr6RMQdKpnYE8BWwR5SXwV7dJJHefSedd7rEsZTHQUUH3U --mini";
+      WorkingDirectory = "%S/monero-p2pool";
+      Restart = "always";
+
+      CapabilityBoundingSet = "";
+      LockPersonality = true;
+      MemoryDenyWriteExecute = true;
+      #NoNewPrivileges = true;
+      PrivateDevices = true;
+      #PrivateTmp = true;
+      DynamicUser = true;
+      PrivateUsers = true;
+      ProcSubset = "pid";
+      ProtectClock = true;
+      ProtectControlGroups = true;
+      ProtectHome = true;
+      ProtectHostname = true;
+      ProtectKernelLogs = true;
+      ProtectKernelModules = true;
+      ProtectKernelTunables = true;
+      ProtectProc = "invisible";
+      #ProtectSystem = "strict";
+      #RemoveIPC = true;
+      RestrictAddressFamilies = [ "AF_INET" "AF_INET6" "AF_UNIX" ];
+      RestrictNamespaces = true;
+      RestrictRealtime = true;
+      StateDirectory = "monero-p2pool";
+      SystemCallArchitectures = "native";
+      SystemCallFilter = "@system-service";
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 18080 18081 ];
 
   environment.persistence = {
-    "/persist".directories = [ "/var/lib/monero" ];
+    "/persist/cache".directories = [
+      "/var/lib/monero"
+      "/var/lib/private/monero-p2pool"
+    ];
   };
 }
