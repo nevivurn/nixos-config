595fd501aa6454bf7c3bf628e46285b847d4f111
Yongun Seong <nevivurn@nevi.dev>
2024-04-20 17:29:07 +0900

    feat(athebyne/audiobookshelf): apply security settings

--- systems/athebyne/services/audiobookshelf.nix
+++ systems/athebyne/services/audiobookshelf.nix
@@ -18,7 +18,31 @@ in {
     }
   '';
 
+  # hardening
+  systemd.services.audiobookshelf.serviceConfig = {
+    CapabilityBoundingSet = "";
+    DynamicUser = true;
+    LockPersonality = true;
+    PrivateDevices = true;
+    PrivateUsers = true;
+    ProcSubset = "pid";
+    ProtectClock = true;
+    ProtectControlGroups = true;
+    ProtectHome = true;
+    ProtectHostname = true;
+    ProtectKernelLogs = true;
+    ProtectKernelModules = true;
+    ProtectKernelTunables = true;
+    ProtectProc = "invisible";
+    RestrictAddressFamilies = [ "AF_INET" "AF_INET6" ];
+    RestrictNamespaces = true;
+    RestrictRealtime = true;
+    SystemCallArchitectures = "native";
+    SystemCallFilter = "@system-service";
+    SystemCallErrorNumber = "EPERM";
+  };
+
   environment.persistence = {
-    "/persist".directories = [ "/var/lib/${cfg.dataDir}" ];
+    "/persist".directories = [ "/var/lib/private/${cfg.dataDir}" ];
   };
 }
