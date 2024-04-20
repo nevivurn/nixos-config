81a1d1e15e9401e8e2eb94d76df6992b8101fcba
Yongun Seong <nevivurn@nevi.dev>
2024-04-20 14:59:37 +0900

    feat(athebyne/home): add weechat

--- systems/athebyne/home/default.nix
+++ systems/athebyne/home/default.nix
@@ -1,7 +1,13 @@
-{ inputs, ... }:
+{ inputs, config, pkgs, ... }:
 
 with inputs;
 
 {
   imports = [ self.homeModules.develop ];
+
+  home.packages = with pkgs; [ weechat ];
+
+  home.persistence."/persist${config.home.homeDirectory}" = {
+    directories = [ ".config/weechat" ".local/share/weechat" ];
+  };
 }
