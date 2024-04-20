768b9fa4df376ed88dd5952aa0048f247a9d7b2d
Yongun Seong <nevivurn@nevi.dev>
2024-04-20 17:19:28 +0900

    feat(athebyne/kavita): add reverse proxy

--- systems/athebyne/services/kavita.nix
+++ systems/athebyne/services/kavita.nix
@@ -2,7 +2,8 @@
 
 with inputs;
 
-{
+let cfg = config.services.kavita.settings;
+in {
   disabledModules = [ "${nixpkgs}/nixos/modules/services/web-apps/kavita.nix" ];
   imports =
     [ "${nixpkgs-unstable}/nixos/modules/services/web-apps/kavita.nix" ];
@@ -11,8 +12,24 @@ with inputs;
     enable = true;
     package = pkgs.pkgsUnstable.kavita;
     tokenKeyFile = "/persist/secrets/kavita-token";
+    settings.IpAddresses = "127.0.0.1";
   };
 
+  services.caddy.virtualHosts."kavita.nevi.network".extraConfig = ''
+    tls {
+      dns cloudflare {env.CF_API_TOKEN}
+      resolvers 1.1.1.1
+    }
+
+    @private remote_ip 192.168.2.0/24 fdbc:ba6a:38de::/64 10.42.42.0/24 fdbc:ba6a:38de:1::/64
+    encode zstd gzip
+
+    handle @private {
+      reverse_proxy localhost:${builtins.toString cfg.Port}
+    }
+  '';
+
+  # hardening
   systemd.services.kavita.serviceConfig = {
     CapabilityBoundingSet = "";
     DynamicUser = true;
