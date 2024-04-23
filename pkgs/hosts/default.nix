22950f6f39cd805c286577cce57de675932bbb57
Yongun Seong <nevivurn@nevi.dev>
2024-04-23 13:37:17 +0900

    refactor(pkgs/hosts): remove rec

--- pkgs/hosts/default.nix
+++ pkgs/hosts/default.nix
@@ -1,13 +1,13 @@
 { stdenvNoCC, fetchFromGitHub }:
 
-stdenvNoCC.mkDerivation rec {
+stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "hosts";
   version = "3.14.68";
 
   src = fetchFromGitHub {
     owner = "StevenBlack";
     repo = "hosts";
-    rev = version;
+    rev = finalAttrs.version;
     hash = "sha256-urlGR/xrjoAsxDz/PzXCO65u0oytkVM6l9FV21NO9ic=";
   };
 
@@ -16,4 +16,4 @@ stdenvNoCC.mkDerivation rec {
     install -Dm644 -t $out hosts
     runHook postInstall
   '';
-}
+})
