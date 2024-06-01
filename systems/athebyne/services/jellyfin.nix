7bc4427d2cc03e6b56892eb3df4f973f341c6774
Yongun Seong <nevivurn@nevi.dev>
2024-06-01 15:22:55 +0900

    feat(athebyne/jellyfin): make datadir rw

--- systems/athebyne/services/jellyfin.nix
+++ systems/athebyne/services/jellyfin.nix
@@ -1,3 +1,8 @@
+{ config, ... }:
+
+let
+  cfg = config.services.jellyfin;
+in
 {
   services.jellyfin = {
     enable = true;
@@ -9,6 +14,7 @@
     ProcSubset = "pid";
     ProtectHome = true;
     ProtectSystem = "strict";
+    ReadWritePaths = [ cfg.dataDir ];
   };
 
   environment.persistence = {
