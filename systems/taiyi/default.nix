274f70f7d17b29954f36b8a2b02238e7fc1a678c
Yongun Seong <nevivurn@nevi.dev>
2024-06-10 18:48:41 +0900

    feat(taiyi): test removing crash workarounds
    Seems to be addressed by BIOS changes

--- systems/taiyi/default.nix
+++ systems/taiyi/default.nix
@@ -76,13 +76,6 @@ in
 
   ## Boot
 
-  # try to address crashes
-  # ref: https://bugzilla.kernel.org/show_bug.cgi?id=206903#c322
-  boot.kernelParams = [
-    "amdgpu.ppfeaturemask=0xffffbffd"
-    "iommu=pt"
-  ];
-
   boot.initrd.postDeviceCommands = lib.mkAfter ''
     zfs rollback rpool/local/root@empty
   '';
