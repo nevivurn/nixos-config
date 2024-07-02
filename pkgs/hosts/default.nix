b447f8300dffbdfc774e9a7c4090e62f98d25c98
Yongun Seong <nevivurn@nevi.dev>
2024-07-03 01:26:25 +0900

    hosts: 3.14.82 -> 3.14.83
    Diff: https://github.com/StevenBlack/hosts/compare/3.14.82...3.14.83

--- pkgs/hosts/default.nix
+++ pkgs/hosts/default.nix
@@ -2,13 +2,13 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "hosts";
-  version = "3.14.82";
+  version = "3.14.83";
 
   src = fetchFromGitHub {
     owner = "StevenBlack";
     repo = "hosts";
     rev = finalAttrs.version;
-    hash = "sha256-FS9+w+9QPBd6hCtX7C5x/xm4nGCA0lOtYgjefkQNbbg=";
+    hash = "sha256-ku7ltZ7cw8n+PH49fIeHMFFPMVZKqwQKVoLhWv4hrxg=";
   };
 
   installPhase = ''
