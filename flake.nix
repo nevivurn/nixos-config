6abad97861c986d8825cb83055a3a3c2390ccec0
Yongun Seong <nevivurn@nevi.dev>
2024-08-07 17:38:48 +0900

    feat(flake): add all-configs check

--- flake.nix
+++ flake.nix
@@ -1,7 +1,5 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
@@ -19,20 +17,36 @@
   };
 
   outputs =
-    {
-      self,
-      flake-utils,
-      nixpkgs,
-      ...
-    }@inputs:
+    { self, nixpkgs, ... }@inputs:
     let
       systems = [
         "x86_64-linux"
         "aarch64-darwin"
       ];
       inherit (nixpkgs) lib;
+
+      # from flake-utils
+      eachSystem =
+        f:
+        let
+          op =
+            attrs: system:
+            let
+              ret = f system;
+              op =
+                attrs: key:
+                attrs
+                // {
+                  ${key} = (attrs.${key} or { }) // {
+                    ${system} = ret.${key};
+                  };
+                };
+            in
+            builtins.foldl' op attrs (builtins.attrNames ret);
+        in
+        builtins.foldl' op { } systems;
     in
-    flake-utils.lib.eachSystem systems (
+    eachSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -46,15 +60,21 @@
         packages = import ./pkgs pkgs;
 
         apps = {
-          nvd-diff = flake-utils.lib.mkApp {
-            drv = pkgs.writeScriptBin "nvd-diff" ''
-              nixos-rebuild build
-              ${lib.getExe pkgs.nvd} diff /run/current-system ./result
-            '';
+          nvd-diff = {
+            type = "app";
+            program =
+              let
+                pkg = pkgs.writeScriptBin "nvd-diff" ''
+                  nixos-rebuild build
+                  ${lib.getExe pkgs.nvd} diff /run/current-system ./result
+                '';
+              in
+              lib.getExe pkg;
           };
 
-          nix-update = flake-utils.lib.mkApp {
-            drv =
+          nix-update = {
+            type = "app";
+            program =
               let
                 isUpdatable = p: p ? version && lib.hasPrefix self.outPath (builtins.unsafeGetAttrPos "src" p).file;
                 upPkgs = lib.flatten (
@@ -65,15 +85,16 @@
                     )
                   ) systems
                 );
+                pkg = pkgs.writeScriptBin "nix-update" (
+                  ''
+                    nix flake update --commit-lock-file
+                  ''
+                  + (lib.concatMapStringsSep "\n" (
+                    ps: "${lib.getExe pkgs.nix-update} -F --commit --system ${ps.system} ${ps.name}"
+                  ) upPkgs)
+                );
               in
-              pkgs.writeScriptBin "nix-update" (
-                ''
-                  nix flake update --commit-lock-file
-                ''
-                + (lib.concatMapStringsSep "\n" (
-                  ps: "${lib.getExe pkgs.nix-update} -F --commit --system ${ps.system} ${ps.name}"
-                ) upPkgs)
-              );
+              lib.getExe pkg;
           };
         };
       }
@@ -85,12 +106,17 @@
       # Ensure systems evaluate correctly
       checks = lib.genAttrs systems (
         system:
-        lib.mapAttrs (_: c: c.config.system.build.toplevel) (
-          lib.filterAttrs (_: c: c.pkgs.system == system) self.nixosConfigurations
-        )
-        // lib.mapAttrs (_: c: c.system) (
-          lib.filterAttrs (_: c: c.pkgs.system == system) self.darwinConfigurations
-        )
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          configs =
+            lib.mapAttrs (_: c: c.config.system.build.toplevel) (
+              lib.filterAttrs (_: c: c.pkgs.system == system) self.nixosConfigurations
+            )
+            // lib.mapAttrs (_: c: c.system) (
+              lib.filterAttrs (_: c: c.pkgs.system == system) self.darwinConfigurations
+            );
+        in
+        configs // { allConfigs = pkgs.linkFarmFromDrvs "all-configs" (builtins.attrValues configs); }
       );
 
       nixosModules = {
