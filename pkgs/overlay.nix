e3ed684c82c5ef54b4d7f161f7abce64ab056089
Yongun Seong <nevivurn@nevi.dev>
2024-10-13 19:57:32 +0900

    fix(ci): remove ifd

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -76,8 +76,5 @@ import ./default.nix prev
   };
 
   # until https://github.com/NixOS/nixpkgs/pull/348035 gets merged
-  vscode-langservers-extracted = final.callPackage (final.fetchurl {
-    url = "https://raw.githubusercontent.com/NixOS/nixpkgs/2c371c03bab48efeb635b7217e5b4f99322b24f9/pkgs/development/tools/language-servers/vscode-langservers-extracted/default.nix";
-    hash = "sha256-ygKuElbaZTXQv1/wLD/TAUO3GOjeJg/UHiCPV2ZHRMQ=";
-  }) { };
+  vscode-langservers-extracted = final.callPackage ./vscode-langservers-extracted { };
 }
