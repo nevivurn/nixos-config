d217af1422eeadce8a2e6629122090383893d804
Yongun Seong <nevivurn@nevi.dev>
2024-10-20 20:05:33 +0900

    fix(home/sway): force stable webkit due to mesa version mismatch issue

--- home/profiles/sway/default.nix
+++ home/profiles/sway/default.nix
@@ -42,8 +42,10 @@ in
     mupdf
     yt-dlp
 
-    pkgsUnstable.gnucash
-    pkgsUnstable.portfolio
+    # otherwise we run into issues with mesa mismatch
+    # and people don't like it when we backport to stable
+    (pkgsUnstable.gnucash.override { webkitgtk_4_0 = webkitgtk; })
+    (pkgsUnstable.portfolio.override { webkitgtk_4_0 = webkitgtk; })
 
     liquidctl
     moonlight-qt
