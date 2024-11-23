f289154496394c2841ae20088e6d2f2c6911f018
Yongun Seong <nevivurn@nevi.dev>
2024-11-23 19:23:12 +0900

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/2f23fa308a7c067e52dfcc30a0758f47043ec176' (2024-09-22)
      → 'github:nix-community/home-manager/f8831cc700030e11fc91da9ef6270593e6440edc' (2024-11-22)
    • Updated input 'home-manager-darwin':
        'github:nix-community/home-manager/2f23fa308a7c067e52dfcc30a0758f47043ec176' (2024-09-22)
      → 'github:nix-community/home-manager/f8831cc700030e11fc91da9ef6270593e6440edc' (2024-11-22)
    • Updated input 'nix-darwin':
        'github:LnL7/nix-darwin/edabc790a834326dcb5810e2698fa743483510d0' (2024-11-21)
      → 'github:LnL7/nix-darwin/698a62c628c2ec423aa770d8ec0e1d0bcf4fca1a' (2024-11-23)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/e8c38b73aeb218e27163376a2d617e61a2ad9b59' (2024-11-16)
      → 'github:NixOS/nixpkgs/057f63b6dc1a2c67301286152eb5af20747a9cb4' (2024-11-16)
    • Updated input 'nixpkgs-darwin':
        'github:NixOS/nixpkgs/c128e44a249d6180740d0a979b6480d5b795c013' (2024-11-06)
      → 'github:NixOS/nixpkgs/8d48200ead5adea71485965f92405575c66dab04' (2024-11-19)

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
 
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager-darwin.url = "github:nix-community/home-manager/release-24.05";
+    home-manager-darwin.url = "github:nix-community/home-manager/release-24.11";
     home-manager-darwin.inputs.nixpkgs.follows = "nix-darwin/nixpkgs";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
