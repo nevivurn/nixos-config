de16023e7131c78b0bb47da22c8bf5d73c882fb5
Yongun Seong <nevivurn@nevi.dev>
2026-02-21 21:53:54 +0900

    refactor(pkgs): remove hosts

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,9 +8,6 @@ in
   # custom passmenu allowing generic tools for dmenu / xdotool alternatives
   passmenu = callPackage ./passmenu { };
 
-  # hosts list for malware, ads
-  hosts = callPackage ./hosts { };
-
   # ssh-keygen wrapper for for gpg-agent
   ssh-keygen-gpg-wrapper = callPackage ./ssh-keygen-gpg-wrapper { };
 }
