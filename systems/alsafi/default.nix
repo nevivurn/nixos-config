92012175f85090a0283d75771b38e4a67ce44181
Yongun Seong <nevivurn@nevi.dev>
2026-02-28 20:55:15 +0900

    feat(alsafi): add alsafi wg-home

--- systems/alsafi/default.nix
+++ systems/alsafi/default.nix
@@ -106,12 +106,118 @@ in
   networking.domain = "nevi.network";
   networking.timeServers = [ ];
 
-  systemd.network.networks = {
-    "20-wifi" = {
-      matchConfig.Type = "wlan";
-      networkConfig.DHCP = "yes";
+  systemd.network = {
+    netdevs = {
+      "41-wg-proxy" = {
+        netdevConfig = {
+          Name = "wg41";
+          Kind = "wireguard";
+        };
+        wireguardConfig = {
+          PrivateKeyFile = "/persist/secrets/wg-home-wg41-priv";
+          RouteTable = 4040;
+          FirewallMark = 4040;
+          RouteMetric = 4041;
+        };
+        wireguardPeers = [
+          {
+            Endpoint = "rtr01.pub.nevi.network:51820";
+            PersistentKeepalive = 25;
+            AllowedIPs = [
+              "0.0.0.0/0"
+              "::/0"
+            ];
+            PublicKey = "Ph7zW70iiZC2hOcxSjI5SBZptUogFcGCTBFjWCvXGUQ=";
+            PresharedKeyFile = "/persist/secrets/wg-home-wg41-alrakis-psk";
+          }
+        ];
+      };
+      "42-wg-proxy" = {
+        netdevConfig = {
+          Name = "wg42";
+          Kind = "wireguard";
+        };
+        wireguardConfig = {
+          PrivateKeyFile = "/persist/secrets/wg-home-wg42-priv";
+          RouteTable = 4040;
+          FirewallMark = 4040;
+          RouteMetric = 4042;
+        };
+        wireguardPeers = [
+          {
+            Endpoint = "rtr02.pub.nevi.network:51820";
+            PersistentKeepalive = 25;
+            AllowedIPs = [
+              "0.0.0.0/0"
+              "::/0"
+            ];
+            PublicKey = "g50BReFg/OqPJFcVdLq280OrgfvoMMttMaOuvDIHdys=";
+            PresharedKeyFile = "/persist/secrets/wg-home-wg42-alrakis-psk";
+          }
+        ];
+      };
+    };
+    networks = {
+      "20-wifi" = {
+        matchConfig.Type = "wlan";
+        networkConfig.DHCP = "yes";
+        networkConfig.Domains = [ "~pub.nevi.network" ];
+      };
+      "41-wg-home" = {
+        matchConfig.Name = "wg41";
+        networkConfig = {
+          Address = [
+            "10.64.41.4/32"
+            "fdbc:ba6a:38de:41::4/128"
+          ];
+          DNS = [
+            "10.64.20.4"
+            "10.64.20.5"
+          ];
+          DNSDefaultRoute = true;
+          Domains = "~.";
+        };
+        routingPolicyRules = [
+          {
+            Family = "both";
+            FirewallMark = 4040;
+            InvertRule = true;
+            Table = 4040;
+            Priority = 4041;
+          }
+        ];
+      };
+      "42-wg-home" = {
+        matchConfig.Name = "wg42";
+        networkConfig = {
+          Address = [
+            "10.64.42.4/32"
+            "fdbc:ba6a:38de:42::4/128"
+          ];
+          DNS = [
+            "10.64.20.4"
+            "10.64.20.5"
+          ];
+          DNSDefaultRoute = true;
+          Domains = "~.";
+        };
+        routingPolicyRules = [
+          {
+            Family = "both";
+            FirewallMark = 4040;
+            InvertRule = true;
+            Table = 4040;
+            Priority = 4042;
+          }
+        ];
+      };
     };
   };
+
+  # allow wg traffic on weird return paths
+  networking.firewall.extraReversePathFilterRules = ''
+    udp sport 51820 accept
+  '';
 
   networking.wireless.iwd.enable = true;
   networking.wireless.interfaces = [ "wlan0" ];
