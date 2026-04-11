0bcfb5cbd05bd9ddb3249b2c985343c61bb1f7c4
Yongun Seong <nevivurn@nevi.dev>
2026-04-11 21:06:51 +0900

    feat(gnucash): remove patches, merged upstream

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -1,9 +1,3 @@
 final: prev:
 
 import ./default.nix prev
-// {
-  # Remove fractional units for KRW
-  gnucash = prev.gnucash.overrideAttrs (prev: {
-    patches = prev.patches ++ [ ./gnucash/krw-no-fraction.patch ];
-  });
-}
