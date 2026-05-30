6d027e9aeda12b30a33bec5778a3fb2b354936b9
Yongun Seong <nevivurn@nevi.dev>
2026-05-31 03:55:39 +0900

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/3ee51fbdac8c8bdfe1e7e1fcaba6520a563f394f?narHash=sha256-QOD/CNm196nCJRheux/URi4/HE66fthdOMqCJoPP1Y0%3D' (2026-05-23)
      → 'github:nix-community/home-manager/b179bde238977f7d4454fc770b1a727eaf55111c?narHash=sha256-RUkMrREjKDQrA%2BdA9%2BxZviGAxM5W1aVdyOr/bSYpHrE%3D' (2026-05-25)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/25f538306313eae3927264466c70d7001dcea1df?narHash=sha256-ZsIrKmhp4vbBXoXXmR/tBXA/UCsAQiJL9vsgZEduhVY%3D' (2026-05-26)
      → 'github:NixOS/nixpkgs/ec942ba042dad5ef097e2ef3a3effc034241f011?narHash=sha256-R5nauXyqyfRUFiZycFFZdkF7wl6eaUpPLst35%2B2nJQY%3D' (2026-05-28)

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
