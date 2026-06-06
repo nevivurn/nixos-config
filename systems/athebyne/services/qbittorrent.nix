03c8ef6c8919b101e26034d05fcbf243fbde4e2a
Yongun Seong <nevivurn@nevi.dev>
2026-06-06 22:56:56 +0900

    feat(athebyne/qbittorrent): apply rebased patch for bulk add

--- systems/athebyne/services/qbittorrent.nix
+++ systems/athebyne/services/qbittorrent.nix
@@ -1,6 +1,20 @@
+{ pkgs, ... }:
+
 {
-  services.qbittorrent.enable = true;
-  services.qbittorrent.saveDir = "/data/torrents";
+  services.qbittorrent = {
+    enable = true;
+    package = pkgs.qbittorrent-nox.overrideAttrs (prevAttrs: {
+      patches = (if prevAttrs ? patches then prevAttrs.patches else [ ]) ++ [
+        (pkgs.fetchpatch2 {
+          # ref: https://github.com/qbittorrent/qBittorrent/pull/24386
+          name = "multi-torrent-dialogue.patch";
+          url = "https://github.com/nevivurn/qBittorrent/compare/release-5.2.1...nevivurn:qBittorrent:rebased/tom/shared-download-window.patch?full_index=1";
+          hash = "sha256-dNbnHJSjtyyS8LKbQahGtWEtx9VEhkSDze1+uUIdPLo=";
+        })
+      ];
+    });
+    saveDir = "/data/torrents";
+  };
 
   networking.firewall.allowedTCPPorts = [ 5555 ];
   networking.firewall.allowedUDPPorts = [ 5555 ];
