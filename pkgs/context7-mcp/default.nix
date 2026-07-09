10e7879361380f28cd9a91f34d271f891d2969e3
Yongun Seong <nevivurn@nevi.dev>
2026-07-09 23:38:43 +0900

    context7-mcp: 3.2.2 -> 3.2.3
    Diff: https://github.com/upstash/context7/compare/@upstash/context7-mcp@3.2.2...@upstash/context7-mcp@3.2.3

--- pkgs/context7-mcp/default.nix
+++ pkgs/context7-mcp/default.nix
@@ -10,13 +10,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "context7-mcp";
-  version = "3.2.2";
+  version = "3.2.3";
 
   src = fetchFromGitHub {
     owner = "upstash";
     repo = "context7";
     tag = "@upstash/context7-mcp@${finalAttrs.version}";
-    hash = "sha256-v3zKMnZsc0gWVAdgZwFTskWcFVlOU6sG2i+qDwjx+dw=";
+    hash = "sha256-yyz4UraRm1JR/C7J2ib0nBU6zsNpKCWIWduTu7OlebM=";
   };
 
   nativeBuildInputs = [
