54f53645086e855ded5b644270805f10d30f66c5
Yongun Seong <nevivurn@nevi.dev>
2026-09-05 21:43:07 +0900

    fix(athebyne): fix hashes

--- systems/athebyne/services/caddy.nix
+++ systems/athebyne/services/caddy.nix
@@ -8,7 +8,7 @@ in
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
-      hash = "sha256-F7d4HwM4oCkQrFMr4SFSC0r52ONxY+PW6z5BJawW8Ok=";
+      hash = "sha256-x07/LsS9ulcPxlCATI8bmANeI8mZcNEAegczffDc2jQ=";
     };
 
     email = "yseong.p@gmail.com";
